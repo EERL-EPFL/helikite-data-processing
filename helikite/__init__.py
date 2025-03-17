3e3cfa14c6aedf9a3f32b776cb4aac538ae05ca4
Evan Thomas <evan.thomas@epfl.ch>
2025-03-17 13:37:07 +0100

    Remove overriden __name__ value in helikite/__init__.py

--- helikite/__init__.py
+++ helikite/__init__.py
@@ -2,5 +2,5 @@
 from helikite.classes.cleaning import Cleaner  # noqa
 
 __version__ = "1.1.0"
-__name__ = "helikite-data-processing"
+__appname__ = "helikite-data-processing"
 __description__ = "Library to generate quicklooks and data quality checks on Helikite campaigns"
